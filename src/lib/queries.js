5bbccc8d58ba3d920e9262bd3937cadcd06ea19f
Lucira Jewelry <tech@lucirajewelry.com>
2026-04-16 17:33:38 +0530

    sorted in descending order

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,6 +1,6 @@
 export const GET_ARTICLES = `
 query getArticles($cursor: String) {
-  articles(first: 12, after: $cursor) {
+  articles(first: 12, after: $cursor,  sortKey: PUBLISHED_AT, reverse: true) {
     edges {
       cursor
       node {
