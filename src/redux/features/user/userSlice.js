405015a1ccd6d809c2f884fb1c9c6b2b00e736e7
amitjha <amitjha25in@gmail.com>
2026-04-23 18:54:19 +0530

    pincode

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: null,
   isAuthenticated: false,
+  pincode: "",
   referralLink: "",
   referralLoading: false,
   referralError: null,
@@ -15,6 +16,9 @@ const userSlice = createSlice({
     login: (state, action) => {
       state.user = action.payload;
       state.isAuthenticated = true;
+    },
+    setPincode: (state, action) => {
+      state.pincode = action.payload;
     },
     logout: (state) => {
       state.user = null;
@@ -38,7 +42,7 @@ const userSlice = createSlice({
   },
 });
 
-export const { login, logout, setAvatar, setReferralLoading, setReferralLink, setReferralError } = userSlice.actions;
+export const { login, setPincode, logout, setAvatar, setReferralLoading, setReferralLink, setReferralError } = userSlice.actions;
 export default userSlice.reducer;
 
 export const selectUser = (state) => state.user.user;
