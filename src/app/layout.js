b25de63c909f3b565c809d1bdcb246c53e2be4b2
bhavnahar-lucira <bhav.nahar@lucirajewelry.com>
2026-05-04 14:13:42 +0530

    datalayer prod

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,8 +36,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  //const isProd = process.env.NODE_ENV === "production";
-    const isProd = true;
+  const isProd = process.env.NODE_ENV === "production";
   return (
     <html lang="en">
       <head>
