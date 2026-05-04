7e1ad3ce729eb9dfcd716ff1ea346911411ed371
Tahir Kutty <tahir.kutty@gmail.com>
2026-05-04 16:49:05 +0530

    marquee object added

--- src/components/home/FeaturedIn.jsx
+++ src/components/home/FeaturedIn.jsx
@@ -103,6 +103,102 @@ export default function FeaturedIn() {
       url: "https://www.indianjeweller.in/Indian-Jewellery-News/15493/lucira-launches-with-debut-collection-celebrating-modern-womanhood-and-motion",
       image: "/images/feature/IndianJeweller.svg",
     },
+    {
+      url: "https://mediabrief.com/candere-founder-rupesh-jain-launches-lab-grown-diamond-brand-lucira/",
+      image: "/images/feature/MediaBrief.svg",
+    },
+    {
+      url: "https://jewelbuzz.in/candere-founder-rupesh-jain-launches-lab-grown-diamond-jewelry-brand-lucira-taps-into-booming-industry-potential/",
+      image: "/images/feature/JewelBuzz.svg",
+    },
+    {
+      url: "https://www.imagesbof.in/candere-founder-rupesh-jain-launches-lab-grown-diamond-jewellery-brand-lucira/",
+      image: "/images/feature/BQF.svg",
+    },
+    {
+      url: "https://retail.economictimes.indiatimes.com/news/apparel-fashion/jewellery/candere-founder-rupesh-jain-launches-lab-grown-diamond-jewelry-brand-lucira/120573620",
+      image: "/images/feature/ET.svg",
+    },
+    {
+      url: "https://madeinmedia.in/candere-founder-rupesh-jain-launches-lab-grown-diamond-jewellery-brand-lucira-taps-into-booming-industry-potential/",
+      image: "/images/feature/MadeInMedia.svg",
+    },
+    {
+      url: "https://startuptalky.com/news/lucira-secures-5-5-million-in-seed-funding/",
+      image: "/images/feature/StartupTalky.svg",
+    },
+    {
+      url: "https://india.entrepreneur.com/news-and-trends/from-candere-to-lucira-jains-new-chapter-in-purposeful/490616",
+      image: "/images/feature/Entrepreneur.svg",
+    },
+    {
+      url: "https://inc42.com/startups/how-rupesh-jains-lucira-wants-to-redefine-luxury-with-lab-grown-diamonds/",
+      image: "/images/feature/Inc42.svg",
+    },
+    {
+      url: "https://www.bwdisrupt.com/article/lucira-founder-rupesh-jain-believes-indias-jewellery-sector-will-be-defined-by-co-existence-not-competition-556873",
+      image: "/images/feature/Disrupt.svg",
+    },
+    {
+      url: "https://www.indiaretailing.com/2025/05/26/rupesh-jains-new-venture-lucira-bets-on-solitaires-targets-offline-launch-global-entry/",
+      image: "/images/feature/IndiaRetailing.svg",
+    },
+    {
+      url: "https://www.indulgexpress.com/fashion/new-launches/2025/May/29/lucira-redefines-fine-jewellery-with-lab-grown-diamonds-and-sustainable-luxury",
+      image: "/images/feature/Indulge.svg",
+    },
+    {
+      url: "https://www.indianjeweller.in/Indian-Jewellery-News/15493/lucira-launches-with-debut-collection-celebrating-modern-womanhood-and-motion",
+      image: "/images/feature/IndianJeweller.svg",
+    },
+    {
+      url: "https://mediabrief.com/candere-founder-rupesh-jain-launches-lab-grown-diamond-brand-lucira/",
+      image: "/images/feature/MediaBrief.svg",
+    },
+    {
+      url: "https://jewelbuzz.in/candere-founder-rupesh-jain-launches-lab-grown-diamond-jewelry-brand-lucira-taps-into-booming-industry-potential/",
+      image: "/images/feature/JewelBuzz.svg",
+    },
+    {
+      url: "https://www.imagesbof.in/candere-founder-rupesh-jain-launches-lab-grown-diamond-jewellery-brand-lucira/",
+      image: "/images/feature/BQF.svg",
+    },
+    {
+      url: "https://retail.economictimes.indiatimes.com/news/apparel-fashion/jewellery/candere-founder-rupesh-jain-launches-lab-grown-diamond-jewelry-brand-lucira/120573620",
+      image: "/images/feature/ET.svg",
+    },
+    {
+      url: "https://madeinmedia.in/candere-founder-rupesh-jain-launches-lab-grown-diamond-jewellery-brand-lucira-taps-into-booming-industry-potential/",
+      image: "/images/feature/MadeInMedia.svg",
+    },
+    {
+      url: "https://startuptalky.com/news/lucira-secures-5-5-million-in-seed-funding/",
+      image: "/images/feature/StartupTalky.svg",
+    },
+    {
+      url: "https://india.entrepreneur.com/news-and-trends/from-candere-to-lucira-jains-new-chapter-in-purposeful/490616",
+      image: "/images/feature/Entrepreneur.svg",
+    },
+    {
+      url: "https://inc42.com/startups/how-rupesh-jains-lucira-wants-to-redefine-luxury-with-lab-grown-diamonds/",
+      image: "/images/feature/Inc42.svg",
+    },
+    {
+      url: "https://www.bwdisrupt.com/article/lucira-founder-rupesh-jain-believes-indias-jewellery-sector-will-be-defined-by-co-existence-not-competition-556873",
+      image: "/images/feature/Disrupt.svg",
+    },
+    {
+      url: "https://www.indiaretailing.com/2025/05/26/rupesh-jains-new-venture-lucira-bets-on-solitaires-targets-offline-launch-global-entry/",
+      image: "/images/feature/IndiaRetailing.svg",
+    },
+    {
+      url: "https://www.indulgexpress.com/fashion/new-launches/2025/May/29/lucira-redefines-fine-jewellery-with-lab-grown-diamonds-and-sustainable-luxury",
+      image: "/images/feature/Indulge.svg",
+    },
+    {
+      url: "https://www.indianjeweller.in/Indian-Jewellery-News/15493/lucira-launches-with-debut-collection-celebrating-modern-womanhood-and-motion",
+      image: "/images/feature/IndianJeweller.svg",
+    },
   ]
 
   return (
