c9615f364263250d2729ae55cb1c657ef5a78acd
Tahir Kutty <tahir.kutty@gmail.com>
2026-05-05 10:51:39 +0530

    Animation added on PDP page, About Us page Fixes, Featured In marquee slider changes

--- src/app/(frontend)/pages/about-our-company/page.jsx
+++ src/app/(frontend)/pages/about-our-company/page.jsx
@@ -7,8 +7,8 @@ import FAQ from "@/components/common/FAQ";
 
 export default function AboutPage() {
     const data = {
-        aboutBlack: "https://www.lucirajewelry.com/cdn/shop/files/about_us_2.png",
-        aboutWhite: "https://www.lucirajewelry.com/cdn/shop/files/about_us_3.png",
+        aboutBlack: "/images/aboutUs/about_us_2.png",
+        aboutWhite: "/images/aboutUs/about_us_3.png",
         founderImage: "https://www.lucirajewelry.com/cdn/shop/files/Group_1321315953_2.png",
         signatureBlack: "https://www.lucirajewelry.com/cdn/shop/files/Group_1321315956_1.png",
         signatureWhite: "https://www.lucirajewelry.com/cdn/shop/files/Group_1321315955_2.png",
@@ -99,18 +99,18 @@ export default function AboutPage() {
         <>
             <section className="relative w-full overflow-hidden text-center py-10 md:py-16 px-4 md:px-5">
                 <div className="max-w-300 mx-auto relative">
-                    <div className="absolute top-12.5 md:top-17.5 left-1/2 -translate-x-1/2 w-full flex justify-center z-0 pointer-events-none">
+                    <div className="absolute -top-2 md:top-17.5 left-1/2 -translate-x-1/2 w-full flex justify-center z-0 pointer-events-none">
                         <div className="relative w-[90%] md:w-250 h-25 md:h-40">
                             <Image src={data.aboutBlack} alt="About Us" fill className="object-contain" priority />
                         </div>
                     </div>
-                    <div className="pt-25 md:pt-35 flex flex-col items-center">
+                    <div className="pt-10 md:pt-35 flex flex-col items-center">
                         <div className="relative w-full max-w-105 flex flex-col items-center mb-10">
                             <div className="absolute inset-0 overflow-hidden z-20 pointer-events-none">
-                                <div className="absolute left-1/2 -translate-x-1/2 -top-15 md:-top-22 w-[90%] md:w-250 h-30 md:h-50">
+                                <div className="absolute left-1/2 -translate-x-1/2 -top-15 md:-top-22 w-[90%] md:w-180 lg:w-250 h-30 md:h-50">
                                     <Image src={data.aboutWhite} alt="About Us White" fill className="object-contain" />
                                 </div>
-                                <div className="absolute -bottom-10 md:-bottom-15 -right-10 md:-right-20 w-25 md:w-37.5 h-25 md:h-37.5">
+                                <div className="absolute -bottom-10 md:-bottom-15 right-0 md:-right-20 w-25 md:w-37.5 h-25 md:h-37.5">
                                     <Image src={data.signatureWhite} alt="Signature White" fill className="object-contain" />
                                 </div>
                             </div>
@@ -118,11 +118,11 @@ export default function AboutPage() {
                             <div className="relative z-10 w-full h-105 md:h-175">
                                 <Image src={data.founderImage} alt={data.founderName} fill className="object-cover rounded" />
                             </div>
-                            <div className="absolute -bottom-10 md:-bottom-15 -right-10 md:-right-20 w-25 md:w-37.5 h-25 md:h-37.5 z-0">
+                            <div className="absolute -bottom-10 md:-bottom-15 right-0 md:-right-20 w-25 md:w-37.5 h-25 md:h-37.5 z-0">
                                 <Image src={data.signatureBlack} alt="Signature Black" fill className="object-contain" />
                             </div>
                             
-                            <div className="absolute bottom-0 left-0 md:-left-42.5 text-left">
+                            <div className="absolute -bottom-10 left-0 md:bottom-0 md:-left-40 lg:-left-52 text-left">
                                 <h3 className="text-[18px] md:text-[25px] font-medium uppercase">
                                     {data.founderName}
                                 </h3>
@@ -139,24 +139,20 @@ export default function AboutPage() {
             </section>
             
             <section className="w-full bg-white py-12 md:py-16 overflow-x-auto">
-                <div className="max-w-325 mx-auto relative">
-                    {/* <div className="absolute top-1/2 left-0 w-full h-[2px] bg-[#b8957a] -translate-y-1/2 z-0"></div> */}
-                    {/* <div className="absolute top-[35%] left-0 w-full h-[2px] bg-[#b8957a] -translate-y-1/2 z-0"></div> */}
-
-                    <div className="absolute top-[35%] left-0 w-full h-0.5 bg-[#b8957a] -translate-y-1/2 z-0 hidden md:block" />
-
-                    <div className="flex md:justify-between items-center gap-8 px-5 relative z-10 min-w-max md:min-w-0">
+                <div className="mx-auto relative">
+                    <div className="absolute top-[35%] left-0 w-full h-0.5 bg-[#b8957a] -translate-y-1/2 z-0 hidden lg:block" />
+                    <div className="flex md:justify-around items-center gap-8 px-5 relative z-10 min-w-max md:min-w-0">
                         {timelineData.map((item, index) => (
                             <div key={index} className="flex flex-col items-center min-w-65">
-                                <div className="text-[16px] md:text-[20px] mb-6">{item.year}</div>
+                                <div className="text-base md:text-xl mb-6">{item.year}</div>
                                 <div className="w-15 md:w-20 h-15 md:h-20 mb-6 relative">
                                     <Image src={item.image} alt={item.title} fill className="object-cover rounded-full border-[3px] border-white shadow-md" />
                                 </div>
                                 <div className="text-center max-w-65">
-                                    <h3 className="text-[16px] md:text-[18px] font-medium uppercase mb-3">
+                                    <h3 className="text-base md:text-lg font-medium uppercase mb-3">
                                         {item.title}
                                     </h3>
-                                    <p className="text-[14px] md:text-[16px] text-gray-500">
+                                    <p className="text-sm md:text-base text-gray-500">
                                         {item.description}
                                     </p>
                                 </div>
@@ -166,7 +162,7 @@ export default function AboutPage() {
                 </div>
             </section>
             <section className="w-full py-10 md:py-16 bg-white">
-                <div className="max-w-350 mx-auto px-4 md:px-8 grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-12">        
+                <div className="max-w-350 mx-auto px-4 md:px-8 grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-12">        
                     <div className="relative flex flex-col gap-6 md:gap-8">            
                         <div className="relative w-full h-75 sm:h-100 md:h-150">
                             <Image 
@@ -240,9 +236,9 @@ export default function AboutPage() {
                 <FeaturedIn />
             </Suspense>            
             <section className="w-full py-10 md:pt-16 md:pb-8">
-                <div className="max-w-300 mx-auto px-6">
-                    <div className="relative w-full rounded-[10px] overflow-hidden">
-                        <div className="relative w-full h-45 xl:h-150 lg:h-125 md:h-100">
+                <div className="max-w-300 mx-auto px-4 lg:px-1">
+                    <div className="relative w-full rounded-lg overflow-hidden">
+                        <div className="relative w-full h-45 lg:h-150 md:h-100">
                             <Image
                                 src="https://www.lucirajewelry.com/cdn/shop/files/WhatsApp_Image_2025-09-24_at_11.43.16_3c4dde0d_1_2_2000x.png?v=1760096833"
                                 alt="Our Team"
@@ -251,11 +247,11 @@ export default function AboutPage() {
                                 priority
                             />
                         </div>
-                        <div className="w-full bg-[#f2f2f2] text-center px-4 py-6 mt-4 xl:absolute xl:inset-0 xl:bg-transparent xl:flex xl:flex-col xl:justify-end xl:items-center xl:text-white xl:px-5 xl:pb-8 xl:mt-0">
+                        <div className="w-full bg-gray-100 text-center px-4 py-6 mt-2 xl:absolute xl:inset-0 xl:bg-transparent xl:flex xl:flex-col xl:justify-end xl:items-center xl:text-white xl:px-5 xl:pb-8 xl:mt-0">
                             <p className="text-[16px] sm:text-[20px] md:text-[24px] font-normal opacity-80">
                                 OUR TEAM, OUR FAMILY
                             </p>
-                            <p className="text-[14px] sm:text-[16px] md:text-[18px] max-w-[90%] md:max-w-200 mt-2 leading-[1.4]">
+                            <p className="text-[14px] sm:text-[16px] md:text-[18px] max-w-[90%] md:max-w-200 mx-auto mt-2 leading-[1.4]">
                                 Designers, Thinkers and Storytellers shaping a new language of luxury.
                             </p>
                         </div>
@@ -266,7 +262,9 @@ export default function AboutPage() {
                 <ExploreCollectionSection />
             </Suspense>
             <Suspense fallback={<div className="h-20 bg-gray-100 animate-pulse"></div>}>
-                <LuciraPromise title="Lucira Promise" description="Where Authenticity meets Trust and Care that lasts a Lifetime" />
+                <div className="mt-10">
+                    <LuciraPromise title="Lucira Promise" description="Where Authenticity meets Trust and Care that lasts a Lifetime" />
+                </div>
             </Suspense>
             <Suspense fallback={<div className="h-20 bg-gray-100 animate-pulse"></div>}>
                 <FAQ title="FAQ'S" description="Got questions? We've got answers. Here's everything you need to know about earning, redeeming, and making the most of your rewards." faqs={faqData} />
