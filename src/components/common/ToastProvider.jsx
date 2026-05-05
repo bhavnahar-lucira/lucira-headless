faae1b409d6bec86d2be0139b2942b7aa58a52f5
Lucira Jewelry <harshtandel103@gmail.com>
2026-05-05 17:36:03 +0530

    ss

--- src/components/common/ToastProvider.jsx
+++ src/components/common/ToastProvider.jsx
@@ -2,6 +2,7 @@
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Info } from "lucide-react";
 
 const SuccessIcon = () => (
   <svg width="9" height="7" viewBox="0 0 9 7" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -12,6 +13,17 @@ const SuccessIcon = () => (
 const ErrorIcon = () => (
   <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M1 9L9 1M1 1L9 9" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+);
+
+const InfoIcon = () => (
+  <Info size={16} strokeWidth={2.5} color="white" />
+);
+
+const WarningIcon = () => (
+  <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 1V8" stroke="white" strokeWidth="2" strokeLinecap="round"/>
+    <circle cx="5" cy="12" r="1.5" fill="white"/>
   </svg>
 );
 
@@ -34,6 +46,12 @@ export default function ToastProvider() {
         }
         if (type === "error") {
           return <ErrorIcon />;
+        }
+        if (type === "warning") {
+          return <WarningIcon />;
+        }
+        if (type === "info" || type === "default") {
+          return <InfoIcon />;
         }
       }}
     />
