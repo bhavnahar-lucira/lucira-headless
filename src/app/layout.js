47f403060908f621e734a2f88842cc533c590b8e
Lucira Jewelry <sumitchavda04@gmail.com>
2026-05-06 17:55:29 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,8 +36,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const isProd = process.env.NODE_ENV === "production";
-  // const isProd = true;
+  // const isProd = process.env.NODE_ENV === "production";
+  const isProd = true;
   return (
     <html lang="en">
       <head>
