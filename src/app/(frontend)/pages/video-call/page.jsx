ef988045f9dbb6f6a56fb86092a3613baff86e94
Lucira Jewelry <aakashrajawat6@gmail.com>
2026-05-07 15:18:25 +0530

    margin adjust

--- src/app/(frontend)/pages/video-call/page.jsx
+++ src/app/(frontend)/pages/video-call/page.jsx
@@ -157,7 +157,7 @@ export default function VideoCallPage() {
       <section className="relative w-full h-[535px] md:h-[605px] flex items-end md:items-center mb-5 md:mb-20 bg-cover bg-center bg-no-repeat bg-[url('https://cdn.shopify.com/s/files/1/0739/8516/3482/files/Video-Call-Banner-Desktop_320cf8bb-6d9a-4e6a-9557-94ef49275b25.jpg?v=1751366655')]">
         <div className="absolute inset-0 bg-black/30 z-10"></div>
 
-        <div className="relative z-20 w-full max-w-[1320px] mx-auto px-5 md:px-10 pb-10 md:pb-0">
+        <div className="relative z-20 w-full max-w-[1320px] px-5 md:px-10 pb-10 md:pb-0">
           <div className="text-center md:text-left text-white max-w-[750px] flex flex-col items-center md:items-start mx-auto md:mx-0">
             <h1 className="font-abhaya text-3xl md:text-[56px] font-bold leading-tight mb-4 tracking-wide uppercase">
               WIDE RANGE OF JEWELRY JUST A CALL AWAY 
